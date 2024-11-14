661829192ea213344859fd90ce8ea21c1689c5e2
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2024-11-13 23:04:05 -0800

    working on the navbar component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar'
+import About from './About'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Navbar/>
+    {/* <About/> */}
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/about">About</NavLink>
+    </>
   )
 }
 
